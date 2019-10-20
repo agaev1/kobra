d4c6ee0883fea53b4667524adac2e5d4cce20be2
Khagani <kagaev1@gmail.com>
2019-10-20 02:47:35 +0200

    Added a change log

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+# Change log for kobra
+___
+
+## [1.3.1] - 2019-10-20
+### Added
+- Includes date functions that use QuantLib
--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ import setuptools
 
 setuptools.setup(
     name="kobra",
-    version="1.2.0",
+    version="1.3.0",
     author="agaev1",
-    description="Helping you get to data faster",
+    description="Helping you get to data faster.",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3.6",
@@ -14,6 +14,7 @@ setuptools.setup(
     install_requires=[
         'QuantLib-Python==1.16.1',
         'pandas==0.25.2',
-        'pytest==5.2.1'
+        'pytest==5.2.1',
+        'dash==1.4.1'
     ]
 )
